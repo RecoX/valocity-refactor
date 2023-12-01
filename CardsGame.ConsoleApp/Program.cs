fe3ca706806fe3311a3bc23ce49ad4c2ec4118cd
Recox <lucas.recoaro@gmail.com>
2023-12-01 20:15:17 +1300

    feat: Added Console Application to use the Deck functionality and display it for the player

---
+++ CardsGame.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using CardsGame.Core.Classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Deck deck = new Deck();
+
+        deck.Shuffle();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Card card = deck.Deal();
+            Console.WriteLine($"Received: {card.Rank} of {card.Suit}");
+        }
+
+        Console.ReadLine();
+    }
+}
